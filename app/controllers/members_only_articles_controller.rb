728c8592e8b0f08aacdaf1db71a50a52c089abab
Eugene <eugeneayonga@gmail.com>
2022-10-08 16:57:52 +0300

    commit tree 6fa928453c137c114661d86c9f4fe730b8cf25cc

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,14 +1,32 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  # def index
+  #   articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+  #   render json: articles, each_serializer: ArticleListSerializer
+  # end
+
+  # def show
+  #   article = Article.find(params[:id])
+  #   render json: article
+  # end
+
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    if session[:user_id]
+      articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer, status: :ok
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if session[:user_id]
+      article = Article.find(params[:id])
+      render json: article, status: :ok
+    else
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 
   private
